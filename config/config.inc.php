fdfc35749ef83f465bd396fadaac162d4de6b2cd
Entropy Knowledge Network <116267948+EntropyKN@users.noreply.github.com>
2023-11-15 22:47:33 +0100

    fix config.inc.php

--- config/config.inc.php
+++ config/config.inc.php
@@ -3,7 +3,7 @@ error_reporting(E_ERROR  | E_PARSE ); //| E_WARNING
 //ini_set("display_errors", "1");error_reporting(E_ALL);
 session_start();
 
-define("API_KEY_GOOGLE_TTS","AIzaSyALLZNgwtanBJHlqFJ8xtUZyd830y9Fmcs");//<== IMPORTANT: insert here your google cloud TTS API KEY!
+define("API_KEY_GOOGLE_TTS","INSERT KEY");//<== IMPORTANT: insert here your google cloud TTS API KEY!
 
 define("SITE_URL_LOCATION","https://ecore.sgameup.com/");// include slash at the end ex: https://sub.domain.com/
 
